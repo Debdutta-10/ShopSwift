dc5a93e183aa406294f4a2c1c15cb2291a642432
Debdutta <debduttamukherjee32@gmail.com>
2023-12-30 01:15:02 +0530

    User Registered Successfully

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoutes.js'
 
 //Configuring dotenv
 dotenv.config();
@@ -14,6 +15,9 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/auth',authRoutes);
 
 //port
 const PORT = process.env.PORT || 8000;
